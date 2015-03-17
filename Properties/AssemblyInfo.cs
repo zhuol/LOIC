9da091e0d585f939db5d7e12e2e31af1aa524f68
Taku <zhuozhuozhuol@gmail.com>
2015-03-17 17:57:51 -0400

    Copy from others

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Low Orbit Ion Cannon")]
+[assembly: AssemblyDescription("TCP/IP stress-test tool")]
+[assembly: AssemblyProduct("Low Orbit Ion Cannon")]
+[assembly: AssemblyCopyright("Public domain")]
+
+[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
+
+[assembly: System.CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.1.1.26")]
